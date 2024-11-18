2b5def8b007b0fe96ad64eb9103792e9b04658eb
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-11-18 17:23:40 +0100

    (WIP): progress

--- utils/helpers.py
+++ utils/helpers.py
@@ -11,7 +11,7 @@ def extract_book_and_author(title):
 
 def extract_recommendations(comment: str) -> List[Tuple[str, bool]]:
     recommendations = []
-    for m in re.finditer("\{\{([^}]+)\}\}|\{([^}]+)\}", comment):
+    for m in re.finditer(r"\{\{([^}]+)\}\}|\{([^}]+)\}", comment):
         group = m.group()
         # handle empty brackets, general way to handle them
         cleaned = group.replace("{", "").replace("}", "").replace("*", "").strip()
@@ -20,3 +20,25 @@ def extract_recommendations(comment: str) -> List[Tuple[str, bool]]:
         is_long_version = (group.count("{") + group.count("}")) == 4
         recommendations.append((cleaned, is_long_version))
     return recommendations
+
+
+def split_book_title_and_series(book_title: str) -> Tuple[str, str | None]:
+    series_match = None
+    for series_match in re.finditer(r"\(([^)]+)\)", book_title):
+        pass
+    if series_match is not None:
+        group = series_match.group()
+        book_index_in_series = re.search(r"#\d.", group)
+
+        if book_index_in_series is not None:
+            series_name = group
+            book_title = book_title.replace(series_name, "").strip()
+
+            return (book_title, series_name.replace("(", "").replace(")", ""))
+    return (book_title, None)
+
+
+reslut = split_book_title_and_series(
+    "The Name of the Wind (The Kingkiller Chronicle, #1)"
+)
+print(reslut)
